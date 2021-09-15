021735ad9c78f18916649dafaa0cd84f62e4862f
lukasmeza <lukasmeza484@gmail.com>
2021-09-14 21:39:01 -0300

    progra

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { Usuario } from 'src/app/model/Usuario';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  public usuario: Usuario;
+
+  constructor(
+    private router: Router,
+    private toastController: ToastController
+  ) {
+    this.usuario = new Usuario();
+    this.usuario.nombreUsuario = '';
+    this.usuario.password = '';
+  }
+
+  public ngOnInit(): void {}
+
+  public ingresar(): void {
+    if (!this.validarUsuario(this.usuario)) {
+      return;
+    }
+
+    this.mostrarMensaje('¡Bienvenido!');
+    const navigationExtras: NavigationExtras = {
+      state: {
+        usuario: this.usuario,
+      },
+    };
+    this.router.navigate(['/home'], navigationExtras);
+  }
+
+  public validarUsuario(usuario: Usuario): boolean {
+    const mensajeError = usuario.validarUsuario();
+
+    if (mensajeError) {
+      this.mostrarMensaje(mensajeError);
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Muestra un toast al usuario
+   *
+   * @param mensaje
+   * @param duracion 
+   */
+  async mostrarMensaje(mensaje: string, duracion?: number) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: duracion ? duracion : 2000,
+    });
+    toast.present();
+  }
+}
