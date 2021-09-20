fd4a895cae81df3fd9d5da138415ebce07b2c1f1
lukasmeza <lukasmeza484@gmail.com>
2021-09-19 22:20:57 -0300

    app terminada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'password',
+    loadChildren: () => import('./pages/password/password.module').then( m => m.PasswordPageModule)
+  },
 ];
 
 @NgModule({
