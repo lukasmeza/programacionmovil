ed13d195af0976744979cf5fb4fd9cd1984e1733
alejandroynz <73037725+alejandroynz@users.noreply.github.com>
2021-11-02 16:33:57 -0300

    POPUP RECUPERAR CONTRASEÑA

--- src/app/pages/password/password.page.ts
+++ src/app/pages/password/password.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
+import { Validators, FormControl, FormBuilder, FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-password',
   templateUrl: './password.page.html',
   styleUrls: ['./password.page.scss'],
 })
-export class PasswordPage implements OnInit {
+export class PasswordPage {
 
-  constructor() { }
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  RegisterForm: FormGroup;
 
-  ngOnInit() {
+  valuefromuser: any;
+
+  constructor(
+    private alertCtrl: AlertController
+  ) { }
+  submit(){
+    console.log('value is = ', this.valuefromuser);
   }
 
+
+
+  async ingresar(){
+    await this.alertCtrl.create({
+      header: 'Recuperación de contraseña enviada correctamente.',
+      message: 'Esto podría tardar unos minutos.',
+      buttons: [
+        { text: 'aceptar', handler: (res) => {
+          console.log(res.promo);
+        }
+      }
+      ]
+    }).then( res => res.present());
+  }
 }
