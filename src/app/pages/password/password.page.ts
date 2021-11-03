7d05df26c7382c7c62601b4b0b76994204bf457a
alejandroynz <73037725+alejandroynz@users.noreply.github.com>
2021-11-02 22:35:33 -0300

    UPDATE RECUPERAR CLAVE

--- src/app/pages/password/password.page.ts
+++ src/app/pages/password/password.page.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { Router, NavigationExtras } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+
 
 import { Validators, FormControl, FormBuilder, FormGroup} from '@angular/forms';
+import { Usuario } from 'src/app/model/Usuario';
 @Component({
   selector: 'app-password',
   templateUrl: './password.page.html',
   styleUrls: ['./password.page.scss'],
 })
-export class PasswordPage {
+export class PasswordPage implements OnInit {
+  public usuario: Usuario;
 
   // eslint-disable-next-line @typescript-eslint/naming-convention
   RegisterForm: FormGroup;
@@ -15,15 +20,63 @@ export class PasswordPage {
   valuefromuser: any;
 
   constructor(
-    private alertCtrl: AlertController
-  ) { }
+    private alertCtrl: AlertController,
+    private router: Router,
+    private toastController: ToastController
+  )
+  {
+    this.usuario = new Usuario();
+    this.usuario.email = '';
+   }
+  ngOnInit(): void {}
+
+  public ingresar(): void {
+    if (!this.validarEmail(this.usuario)) {
+      return;
+    }
+
+    this.ingresar2();
+  this.mostrarMensaje('');
+    const navigationExtras: NavigationExtras = {
+      state: {
+        email: this.usuario
+      },
+    };
+  }
+  public validarEmail(email: Usuario): boolean {
+    const mensajeError = email.validarEmail();
+
+    if (mensajeError) {
+      this.mostrarMensaje(mensajeError);
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Muestra un toast al usuario
+   *
+   * @param mensaje
+   * @param duracion
+   */
+   async mostrarMensaje(mensaje: string, duracion?: number) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: duracion ? duracion : 2000,
+    });
+    toast.present();
+  }
+
+
+
   submit(){
     console.log('value is = ', this.valuefromuser);
   }
 
 
 
-  async ingresar(){
+  async ingresar2(){
     await this.alertCtrl.create({
       header: 'Recuperación de contraseña enviada correctamente.',
       message: 'Esto podría tardar unos minutos.',
